5c4e2184c0c19eac7aebf7e932dfafff9f214033
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-03-20 09:57:27 -0300

    Importa components e routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error from './routes/Error';
+
+import Home from './routes/Home';
+import Produto from './routes/Produtos';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    
+      <BrowserRouter>
+       <Nav/>
+          <Routes>
+            <Route path="*" element={<Error />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/produto" element={<Produto />} />
+          </Routes>
+       <Footer/>
+      </BrowserRouter>
+  
   )
 }
 
---
+++ src/routes/Home.jsx
@@ -0,0 +1,8 @@
+import {} from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
