1b7bb8b090ca888e6f9a3bcf9c8314b208c0aece
Joao <jpvieira1312@gmail.com>
2025-03-20 09:42:19 -0300

    criando as arquivos para pasta components e estruturando o arquivo home

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+        <p>   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+            Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Pellentesque auctor neque nec urna. Nulla porta dolor. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+
+            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
+
+            Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nulla nec felis sed leo suscipit sollicitudin. Ut leo metus, sollicitudin sed auctor non, lobortis non orci. Phasellus blandit dui id gravida. Sed urna magna, luctus in gravida a, tempor sit amet dui. Proin dui elit, vulputate sit amet posuere sit amet, vehicula nec risus.
+
+            Aenean ut arcu bibendum, dignissim risus in, viverra justo. Integer vehicula ligula justo, ac rhoncus est pretium eu. Etiam ultricies tincidunt dolor ut luctus. Integer interdum vehicula ultricies. Aenean vitae ante dolor. Pellentesque ullamcorper justo ac magna vehicula tristique. Cras ut erat orci. Nunc viverra velit purus, vitae posuere metus luctus id. Morbi pretium, mi nec porttitor porttitor.
+
+            Turpis lorem tempus ante, vel malesuada libero metus sit amet mi. Nulla tincidunt arcu sapien, et scelerisque magna condimentum vel. Nunc sit amet nisi ac arcu cursus dictum sit amet sed orci. Sed volutpat eros nec pretium tincidunt. Vivamus auctor libero ac libero auctor, et dictum nisl vestibulum. Nulla mollis ligula vel dolor sollicitudin, eu tincidunt lorem ullamcorper.
+
+
+
+            Sed et felis tincidunt, fermentum enim sed, luctus ligula. Mauris sit amet magna augue. Pellentesque lobortis vehicula arcu, nec pretium urna ullamcorper eget. Duis euismod risus a ante mollis, ac condimentum lorem interdum. Ut lobortis nulla ut libero ullamcorper, sit amet posuere arcu euismod.
+
+        </p>
+    </div>
+  )
+}
+
+export default Home
