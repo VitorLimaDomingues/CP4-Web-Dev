5dabb9aa07dea07b5e818482afa883188eda4e1b
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-03-23 23:15:58 -0300

    Projeto CP4-Web-Dev finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Error from './routes/Error';
 
 import Home from './routes/Home';
-import Produto from './routes/PaginaProdutos';
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Sobre from './routes/Sobre'
@@ -17,7 +16,7 @@ function App() {
           <Routes>
             <Route path="*" element={<Error />} />
             <Route path="/" element={<Home />} />
-            <Route path="/produto" element={<Produto />} />
+            <Route path="/produto" element={<PaginaProdutos />} />
             <Route path="/sobre" element={<Sobre />} />
           </Routes>
        <Footer/>
